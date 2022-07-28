b3274e4f56582599d03f5eac54f867309cad2d38
Rodrigo-Conte <103457862+Rodrigo-Conte@users.noreply.github.com>
2022-07-28 11:09:09 -0300

    Update transforma_base.py

--- transforma_base.py
+++ transforma_base.py
@@ -1,1 +1,28 @@
+def transforma_base(quests):
+    dicionario = {}
+    
+    listaf = []
+    listam = []
+    listad = []
+    
+    for i in quests:
+        for quest, nivel in i.items():
+            if nivel == 'facil':
+                listaf.append(i)
 
+            elif nivel == 'medio':
+                listam.append(i)
+            
+            elif nivel == 'dificil':
+                listad.append(i)
+         
+    if len(listaf) != 0:
+        dicionario['facil'] = listaf
+
+    if len(listam) != 0: 
+        dicionario['medio'] = listam
+    
+    if len(listad) != 0:
+        dicionario['dificil'] = listad
+
+    return dicionario
