1ed477d43f913d78e434e3ba84caa36872da25c9
MatheusAquinodeSouza <105607037+MatheusAquinodeSouza@users.noreply.github.com>
2022-07-29 23:05:57 -0300

    Update todos_os_codigos.py

--- todos_os_codigos.py
+++ todos_os_codigos.py
@@ -188,3 +188,13 @@ def questao_para_texto(dic,num):
     final = tracos + inicio + d + e + var_x
 
     return final
+def gera_ajuda(dicionario):
+    lista=[]
+    for quest, resp in dicionario['opcoes'].items():
+        if quest!=dicionario['correta']:
+            lista.append(resp)
+    sort=random.randint(1,2)
+    mod=random.sample(lista, sort)
+    dica='DICA:\n'
+    dica += 'Opções certamente erradas:' + ' | '.join(mod)
+    return dica
