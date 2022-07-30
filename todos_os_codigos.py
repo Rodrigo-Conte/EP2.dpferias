6b4338dd0e0bc9f18c1196bdcf39800ac677b0a0
MatheusAquinodeSouza <105607037+MatheusAquinodeSouza@users.noreply.github.com>
2022-07-29 23:03:47 -0300

    Update todos_os_codigos.py

--- todos_os_codigos.py
+++ todos_os_codigos.py
@@ -169,3 +169,8 @@ import random
 def sorteia_questao(dic, nivel):
     sort=random.choice(dic[nivel])
     return sort
+import random
+def sorteia_questao_inedida(dic, nivel, lista):
+    sort=random.choice(dic[nivel])
+    lista.append(sort)
+    return sort
