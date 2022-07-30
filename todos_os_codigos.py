21a9710110b27d561a512c638e578d7f23db39c9
MatheusAquinodeSouza <105607037+MatheusAquinodeSouza@users.noreply.github.com>
2022-07-29 23:04:46 -0300

    Update todos_os_codigos.py

--- todos_os_codigos.py
+++ todos_os_codigos.py
@@ -174,3 +174,17 @@ def sorteia_questao_inedida(dic, nivel, lista):
     sort=random.choice(dic[nivel])
     lista.append(sort)
     return sort
+def questao_para_texto(dic,num):
+
+    var_x = ''
+    tracos = '------------------------------------- '
+    inicio = 'QUESTAO' + ' ' + str(num) + '\n\n'
+    d = dic['titulo'] + '\n\n'
+    e = 'RESPOSTAS:\n'
+
+    for letra, texto in dic['opcoes'].items():
+        var_x += (f'{letra}: {texto}' '\n')
+    
+    final = tracos + inicio + d + e + var_x
+
+    return final
